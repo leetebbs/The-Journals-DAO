3e42683e4cccd6dcdafc4a24ea3c741684b57cfa
Tebbo <leetebbs@ymail.com>
2023-02-01 17:24:19 +0000

    Textroom changes for spheron

--- pages/textroom.js
+++ pages/textroom.js
@@ -1,6 +1,5 @@
-import styles from '../styles/all.module.scss'
-import { useState, useEffect} from 'react'
-
+import styles from "../styles/all.module.scss";
+import { useState, useEffect } from "react";
 
 import {
   HuddleClientProvider,
@@ -12,59 +11,68 @@ import PeerVideoAudioElem from "../components/PeerVideoAudioElem";
 import MeVideoElem from "../components/MeVideoElem";
 
 const Textroom = () => {
+  const [joined, setJoined] = useState(false);
 
-
-
-  const [joined, setJoined] = useState(false)
-
-  const huddleClient = getHuddleClient("dedb4a37e56b492effabc00442e470df4af70a7a08b0d8a23448a936e4942438");
+  const huddleClient = getHuddleClient(
+    "dedb4a37e56b492effabc00442e470df4af70a7a08b0d8a23448a936e4942438"
+  );
   const peersKeys = useHuddleStore((state) => Object.keys(state.peers));
   const lobbyPeers = useHuddleStore((state) => state.lobbyPeers);
   const roomState = useHuddleStore((state) => state.roomState);
   const recordingState = useHuddleStore((state) => state.recordingState);
   const recordings = useHuddleStore((state) => state.recordings);
-  const isStreaming = useHuddleStore((state) =>state.stream )
- 
-
+  const isStreaming = useHuddleStore((state) => state.stream);
 
   useEffect(() => {
-    if (roomState.joined = true){
+    if ((roomState.joined = true)) {
       setJoined(true);
     }
-  console.log("room state", joined)
-  console.log("is streaming", isStreaming)
-  },[])
- 
+    console.log("room state", joined);
+    console.log("is streaming", isStreaming);
+  }, [joined, isStreaming, roomState]);
+
   const handleJoin = async () => {
     try {
       await huddleClient.join("test", {
-        address: "",
-        wallet: "",
-        ens: "",
+        address: "0x1ABc133C222a185fEde2664388F08ca12C208F76",
+        wallet: "metamask",
+        ens: "tebbo.eth",
       });
 
+      console.log("Client ",huddleClient)
       console.log("joined");
     } catch (error) {
       console.log({ error });
     }
   };
 
-  async function connect(){
+  
+
+  async function connect() {
     huddleClient.enableWebcam();
     handleJoin();
   }
 
   return (
     <HuddleClientProvider value={huddleClient}>
-    <div >
       <div>
-        {/* <h2 className={`text-${!roomState.joined ? "red" : "green"}`}>
+        <div>
+          {/* <h2 className={`text-${!roomState.joined ? "red" : "green"}`}>
           Room Joined:&nbsp;{roomState.joined.toString()}
         </h2> */}
-        <div className={styles.video}>
-        <MeVideoElem/>
-        </div>
-        {/* <h2>Instructions</h2>
+          {joined ? (
+            <div className={styles.video}>
+              <MeVideoElem />
+            </div>
+          ) : (
+            <div className={styles.video}>
+            join
+            </div>
+          )}
+          {/* <div className={styles.video}>
+            <MeVideoElem />
+          </div> */}
+          {/* <h2>Instructions</h2>
         <ol className="w-fit mx-auto text-left">
           <li>
             Click on <b>Enable Stream</b>
@@ -82,28 +90,26 @@ const Textroom = () => {
             the room.
           </li>
         </ol> */}
-      </div>
+        </div>
 
-      <div>
-        <div className="card">
-          <div className={styles.buttonList}>
-
-          
-          {/* <button onClick={handleJoin}>Join Room</button> */}
-          {/* <MeVideoElem /> */}
-          <button onClick={() => connect()}>
-            Join Conference
-          </button>
-          <button onClick={() => huddleClient.disableWebcam()}>
-            Disable Webcam
-          </button>
-          <button onClick={() => huddleClient.enableWebcam()}>
-            Enable Webcam
-          </button>
-          <button onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}>
-            Allow All To Join()
-          </button>
-          {/* <button
+        <div>
+          <div className="card">
+            <div className={styles.buttonList}>
+              {/* <button onClick={handleJoin}>Join Room</button> */}
+              {/* <MeVideoElem /> */}
+              <button onClick={() => connect()}>Join Conference</button>
+              <button onClick={() => huddleClient.disableWebcam()}>
+                Disable Webcam
+              </button>
+              <button onClick={() => huddleClient.enableWebcam()}>
+                Enable Webcam
+              </button>
+              <button
+                onClick={() => huddleClient.allowAllLobbyPeersToJoinRoom()}
+              >
+                Allow All To Join()
+              </button>
+              {/* <button
             onClick={() =>
               // will not work in localhost
               huddleClient.startRecording({
@@ -116,35 +122,34 @@ const Textroom = () => {
           <button onClick={() => huddleClient.stopRecording({ ipfs: true })}>
             stopRecording()
           </button> */}
+            </div>
           </div>
-        </div>
-        
 
-        {lobbyPeers[0] && <h2>Lobby Peers</h2>}
-        <div>
-          {lobbyPeers.map((peer) => (
-            <div>{peer.peerId}</div>
-          ))}
-        </div>
+          {lobbyPeers[0] && <h2>Lobby Peers {lobbyPeers.length}</h2>}
+          <div>
+            {lobbyPeers.map((peer) => (
+              <div>{peer.peerId}</div>
+            ))}
+          </div>
 
-        {peersKeys[0] && <h2>Peers</h2>}
+          {peersKeys[0] && <h2>Peers</h2>}
 
-        <div className={styles.peersgrid}>
-          {peersKeys.map((key) => (
-            <PeerVideoAudioElem key={`peerId-${key}`} peerIdAtIndex={key} />
-          ))}
-        </div>
-        {/* <div className="text-blue">
+          <div className={styles.peersgrid}>
+            {peersKeys.map((key) => (
+              <PeerVideoAudioElem key={`peerId-${key}`} peerIdAtIndex={key} />
+            ))}
+          </div>
+          {/* <div className="text-blue">
           <h2>Recording State</h2>
           <h3>inProgress: {recordingState.inProgress.toString()}</h3>
           <h3>processing: {recordingState.processing.toString()}</h3>
           <h3>started: {recordingState.started.toString()}</h3>
           <h3>recordings: {JSON.stringify(recordings)}</h3>
         </div> */}
+        </div>
       </div>
-    </div>
-  </HuddleClientProvider>
-  )
-}
+    </HuddleClientProvider>
+  );
+};
 
-export default Textroom
+export default Textroom;
