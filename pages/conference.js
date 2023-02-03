994c07e3d7214801a8ec41d5ab42a35a52dbbeb3
Tebbo <leetebbs@ymail.com>
2023-02-03 14:45:10 +0000

    iframe

--- pages/conference.js
+++ pages/conference.js
@@ -1,9 +1,9 @@
 import styles from '../styles/all.module.scss'
 import { HuddleIframe } from "@huddle01/huddle01-iframe";
-let count = 0;
+
 const Conference = () => {
-let count =  count +1;
-console.log(count)
+
+
   const iframeConfig = {
     roomUrl: "https://iframe.huddle01.com/anyvalue",
     height: "100%",
